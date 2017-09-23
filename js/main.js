38a4f81da8d1d4a0a0c900cda2716917ad67ee16
Andrey Gurylev <andrey.gurylev@team.wrike.com>
2017-09-23 04:16:55 +0300

    refactor js, add jsdoc, add eslint

--- js/main.js
+++ js/main.js
@@ -1,26 +1,62 @@
-(function() {
-  const $ = (q) => {
-    const nodes = document.querySelectorAll(q)
-    return nodes.length > 1 ? [...nodes] : nodes[0]
+(function () {
+  /**
+   * jQuery-like syntax for querySelector
+   * 
+   * @param  {!DOMString} query Simple CSS selector
+   * @return {NodeList|Node}    List of nodes or once node of DOM
+   */
+  const $ = (query) => {
+    const nodes = document.querySelectorAll(query)
+    return nodes.length > 1 ? nodes : nodes[0]
   }
 
-  const plural = (n, s1, s2, s5) => (((n % 10) == 1) && ((n % 100) != 11)) ? (s1) : (((((n % 10) >= 2) && ((n % 10) <= 4)) && (((n % 100) < 10) || ((n % 100) >= 20))) ? (s2) : (s5))
+  /**
+   * Pluralize russian words
+   * 
+   * @param  {!number} n Amount of a S
+   * @param  {string} s1 When the S is a multiple of one
+   * @param  {string} s2 When the S is a multiple of two
+   * @param  {string} s5 When the S is a multiple of five
+   * @return {string}    Returns a number with a suitable word
+   */
+  const plural = (n, s1, s2, s5) => (((n % 10) === 1) && ((n % 100) !== 11)) ? (s1) : (((((n % 10) >= 2) && ((n % 10) <= 4)) && (((n % 100) < 10) || ((n % 100) >= 20))) ? (s2) : (s5))
 
-  const pluralYears = (n) => plural(n, 'год', 'года', 'лет')
+  /**
+   * Pluralize years
+   * 
+   * @param  {!number} n                Number of years
+   * @param  {Array<string>} translates Collection with three pluralize translates
+   * @return {string}                   Returns pluralize year
+   */
+  const pluralYears = (n, translates) => plural(n, ...translates)
 
+  /**
+   * Calculate the difference between years
+   * 
+   * @param  {!Date} date End year
+   * @return {number}     Difference
+   */
   const getDiffYears = (date) => {
-    var yearsDiffMs = Date.now() - date.getTime()
-    var yearsDate = new Date(yearsDiffMs)
-    var unixStartYear = new Date(0).getUTCFullYear()
-    return Math.abs(yearsDate.getUTCFullYear() - unixStartYear)
+    const yearsDiffMs = Date.now() - date.getTime() // TODO check typeof Date
+    const yearsDate = new Date(yearsDiffMs)
+    const unixStartYear = 1970
+    return Math.abs(yearsDate.getFullYear() - unixStartYear)
   }
 
-  const renderAge = () => {
-    var nscYears = getDiffYears(new Date(2013, 7, 17))
-    var nscspbYears = getDiffYears(new Date(2015, 5, 21))
+  const renderAge = (node, date) => {
+    const diff = getDiffYears(date)
+    const plural = node.dataset.plural
+    const arPlural = plural.split(',') // TODO check isset(data-plural)
 
-    $('.nsc').textContent = nscYears + ' ' + pluralYears(nscYears)
-    $('.nscspb').textContent = nscspbYears + ' ' + pluralYears(nscspbYears)
+    node.textContent = diff + ' ' + pluralYears(diff, arPlural)
+  }
+
+  const renderAges = () => {
+    const dateStartNodeSchool = new Date(2013, 7, 17)
+    const dateStartNodeSchoolSPb = new Date(2015, 5, 21)
+
+    renderAge($('.nsc'), dateStartNodeSchool)
+    renderAge($('.nscspb'), dateStartNodeSchoolSPb)
   }
 
   const iframeLazyLoad = () => {
@@ -29,9 +65,9 @@
   }
 
   const main = () => {
-    renderAge()
+    renderAges()
     iframeLazyLoad()
   }
 
-  window.addEventListener("load", main)
+  window.addEventListener('load', main)
 })()
